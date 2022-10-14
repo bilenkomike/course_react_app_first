ad9c0dbb811c412f0b199cf0aa6181f96b5632ba
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-10-14 14:11:22 +0300

    useState hooks and handle actions

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,37 @@
+import React , { useState } from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
-function ExponseItem(props) {
-    
+
+
+const ExponseItem = (props) => {
+
+
+    // first elemnt - simple value, second element - value to change first element value
+    const [title, setTitle] = useState(props.title);
+
+    let default_title = props.title;
+    const clickHandler = () => {
+
+        if(title !== 'Updated!') {
+            setTitle('Updated!');
+        }
+        else {
+            setTitle(default_title);
+        }
+        
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
